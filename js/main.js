c554328fc3394dd47c79de93e1a1dba491eab186
Ant <antony@springtimesoft.co.nz>
2019-08-12 16:56:22 +1200

    Add Parent / Child blocks for nav / sub nav

--- js/main.js
+++ js/main.js
@@ -1,11 +1,21 @@
 var $title = $('.js-title');
 var copy   = '.js-copy';
 
+var $child_title = $('.js-child-title');
+var child_copy   = '.js-child-copy';
+
 $title.on('click', function() {
+
   $(this).next(copy).slideToggle();
-  $(this).parent().siblings().children().next().slideUp();
+  $(this).parent().siblings().children().next().toggleClass('active').slideUp();
   return false;
+
 });
 
+$child_title.on('click', function() {
 
+  $(this).next(child_copy).slideToggle();
+  $(this).parent().siblings().children().next().toggleClass('active').slideUp();
+  return false;
 
+});
