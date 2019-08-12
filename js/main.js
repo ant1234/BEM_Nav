2ec39a0d4fb7f67c868fb7e90e364c52c12ceb24
Ant <antony@springtimesoft.co.nz>
2019-08-12 19:00:24 +1200

    Add fonts and active states

--- js/main.js
+++ js/main.js
@@ -1,21 +1,29 @@
-var $title = $('.js-title');
-var copy   = '.js-copy';
+var $parent_title = $('.js-title');
+var parent_copy   = '.js-copy';
 
 var $child_title = $('.js-child-title');
 var child_copy   = '.js-child-copy';
 
-$title.on('click', function() {
+$('.child__copy').hide();
 
-  $(this).next(copy).slideToggle();
+$parent_title.click(function (e) {
+
+  $('.js-title').removeClass('parent-active');
+  $(this).next(parent_copy).slideToggle();
   $(this).parent().siblings().children().next().slideUp();
+  $(this).toggleClass('parent-active');
+  e.stopPropagation();
   return false;
 
 });
 
-$child_title.on('click', function() {
+$child_title.click(function (e) {
 
+  $('.js-child-title').removeClass('child-active');
   $(this).next(child_copy).slideToggle();
   $(this).parent().siblings().children().next().slideUp();
+  $(this).toggleClass('child-active');
+  e.stopPropagation();
   return false;
 
 });
